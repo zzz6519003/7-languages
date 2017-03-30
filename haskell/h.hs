773f7c3bfcc453fb94719a0fcb24ec4d30a9e703
Snowmanzzz <zhengzhongzhao@gmail.com>
2017-03-30 12:52:20 +0800

    add ***

--- haskell/h.hs
+++ haskell/h.hs
@@ -1,8 +1,3 @@
--- Write a list comprehension to build a childhood multiplication table. The table would be a list of
--- three-tuples where the first two are integers from 1–12 and the third is the product of the first two.
-
-mulTable = [(n1, n2, n1 * n2) | let nums = [1..12], n1 <- nums, n2 <- nums]
-
 -- Write a function that takes a list and returns the same list in reverse.
 
 reverseList :: [Integer] -> [Integer]
@@ -25,12 +20,4 @@ allEven3 list = [e | e <- list, even e]
 
 allEven4 :: [Integer] -> [Integer]
 allEven4 list = filter (\x -> even x) list
-
-
-
--- Write a function that builds two-tuples with all possible combinations of two of the colors black,
--- white, blue, yellow, and red. Note that you should include only one of (black, blue) and (blue, black).
-
-colorList :: [([Char], [Char])]
-colorList = [(a, b) | let colors = ["black", "white", "blue", "yellow", "red"], a <- colors, b <- colors, a < b]
 
