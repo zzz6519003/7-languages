b88743d7b40f2a885eb11900106dbb0b1333d36c
Snowmanzzz <zhengzhongzhao@gmail.com>
2017-05-09 22:53:10 +0800

    add day2

--- elm/day2.elm
+++ elm/day2.elm
@@ -29,3 +29,49 @@ yOnClick =
   Signal.map show (Signal.sampleOn Mouse.clicks Mouse.y)
 
 
+-- Use map and signals to draw your own picture at the current mouse position. Change the picture when a mouse button is pressed.
+
+import Graphics.Element exposing(..)
+import Graphics.Collage exposing(..)
+import Color exposing(..)
+import Signal
+import Text exposing(..)
+import Time exposing(every, second)
+import Mouse
+import Window
+
+main = 
+  Signal.map3 show Window.dimensions Mouse.isDown Mouse.position
+
+up   = filled red (circle 24)
+down = filled blue (circle 44)
+
+drawCircle : Int -> Int -> Bool -> Int -> Int -> Form
+drawCircle w h d x y = 
+  case d of 
+    True  -> down
+              |> moveX (normalizeX x w)
+              |> moveY (normalizeY y h)
+    False -> up
+              |> moveX (normalizeX x w)
+              |> moveY (normalizeY y h)
+
+normalizeX : Int -> Int -> Float
+normalizeX x w = 
+  (minmax x w) - toFloat w / 2
+
+normalizeY : Int -> Int -> Float
+normalizeY y h = 
+  0 - ((minmax y h) - toFloat h / 2)
+
+minmax : Int -> Int -> Float
+minmax a d = 
+  if  a < 44 then 44.0
+     else if a > d-44  then toFloat (d - 44) 
+     else toFloat a 
+
+show : (Int,Int) -> Bool -> (Int,Int) -> Element
+show (w,h) d (x,y) = collage w h 
+                [ drawCircle w h d x y ]
+
+
