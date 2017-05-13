f0ee4bac3cf7d04bfbeb9a3a0d508d919b246fec
Snowmanzzz <zhengzhongzhao@gmail.com>
2017-05-13 23:43:00 +0800

    add elixir states

---
+++ elixir/states/lib/states.ex
@@ -0,0 +1,18 @@
+defmodule States do
+  @moduledoc """
+  Documentation for States.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> States.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
