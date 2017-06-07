b25089922f1b6cdce8f2029045850c30e6a4300f
Snowmanzzz <zhengzhongzhao@gmail.com>
2017-06-07 18:40:54 +0800

    add day2 quiz

--- haskell/day-2.hs
+++ haskell/day-2.hs
@@ -1,4 +1,3 @@
-
 -- Write a sort that takes a list and returns a sorted list.
 
 sortList :: [Integer] -> [Integer]
@@ -21,4 +20,25 @@ sortListBy fun (h:t) = insert fun h (sortListBy fun t)
   where
   insert fun i [] = [i]
   insert fun i (sh:st) = if (fun i sh) then i:sh:st else sh:(insert fun i st)
+
+-- Write a sort that takes a list and a function that compares its two arguments and then returns a sorted list.
+
+-- Use a partially applied function to define a function that will return half of a number and another that will append \n to the end of any string.
+
+-- Use a partially applied function to define a function that will
+-- return half of a number and another that will append \n to the
+-- end of any string.
+
+
+-- Define our module.
+module Maine where
+
+
+    -- Curry the division operator with 2. This means that anything
+    -- passed to the halfzies function will be divided by 2.
+    halfzies = (/ 2)
+
+
+    -- Curry the ++ oprator to append the "\n" to any string.
+    newLine = (++ "\n")
 
